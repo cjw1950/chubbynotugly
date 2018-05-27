ef3126bc56db35627d7e886f0146e288f598a16c
Charles Williams <williams_c1950@yahoo.com>
2018-05-27 12:19:12 -0400

    Most recent copy containing react.js

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ https://gist.github.com/LeCoupa/6176077a9a8e2ad00eda
 
 bootstrap
 https://hackerthemes.com/bootstrap-cheatsheet/
+
+handlebars-forms
+https://www.npmjs.com/package/handlebars-form-helpers
+
+
+
+
--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('about',{title:'Chubby Not Ugly',subject:'About Us'});
+  res.render('about',{title:'Chubby Not Ugly',subject:'About Us',image:'./images/singleImage.jpg',text:'About Us I\'m all about motivating women who are like me chubbynotugly is only the beginning it\'s a movement !! Are you with me  An idea, a comment that hurt, a phrase to mask an insecurity ,turned into this I only want to receive and give positive vibes from this day forward -with love Alicia'});
 });
 
 module.exports = router;
